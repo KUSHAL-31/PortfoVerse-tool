26014b8e9afe691d59b4d9b411069c8c38379563
KUSHAL-31 <s.kushal31@gmail.com>
2024-10-22 19:29:26 +0530

    Added packages and folders for frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import PortfolioSteps from "./components/PortfolioSteps/PortfolioSteps";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <p>K31 Portfolio maker</p>
+      <PortfolioSteps />
     </>
   );
 }
