e417629892dccfa03e6f09afe7beb7a597e09c6d
KUSHAL-31 <s.kushal31@gmail.com>
2025-01-16 00:22:30 +0530

    Service screen started

--- client/src/components/pages/UserServices.jsx
+++ client/src/components/pages/UserServices.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 const UserServices = () => {
-  return <div>UserServices</div>;
+  return (
+    <div>
+      <h1>Services</h1>
+      <p>What services do you offer?</p>
+      <input type="text" placeholder="Service 1" />
+      <input type="text" placeholder="Service 2" />
+      <input type="text" placeholder="Service 3" />
+      <input type="text" placeholder="Service 4" />
+      <input type="text" placeholder="Service 5" />
+
+      <h1>Service Details</h1>
+      <p>Describe your services in detail.</p>
+      <textarea placeholder="Service 1 Details" />
+      <textarea placeholder="Service 2 Details" />
+      <textarea placeholder="Service 3 Details" />
+      <textarea placeholder="Service 4 Details" />
+      <textarea
+        placeholder="Service
+    5 Details"
+      />
+    </div>
+  );
 };
 
 export default UserServices;
